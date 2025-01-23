a48b3d61b2920bec74e9a49198e843cbf903184e
Robert Burnett <dr.slate2@gmail.com>
2025-01-23 16:24:57 -0600

    get some basic enemies with pathfinding working

---
+++ src/dummy_backend.c
@@ -0,0 +1,44 @@
+#include "backend.h"
+#include "pimbs/src/allocator.h"
+
+Action dummy_input(struct Backend self) {
+    (void)self;
+    return ACTION_QUIT;
+}
+
+void dummy_render(struct Backend self, RenderEvent cmd) {
+    (void)self;
+    (void)cmd;
+}
+
+void dummy_begin_rendering(struct Backend self) {
+    (void)self;
+}
+
+void dummy_finish_rendering(struct Backend self) {
+    (void)self;
+}
+
+void dummy_deinit(struct Backend self) {
+    (void)self;
+}
+
+void dummy_display(struct Backend self, const char * fmt, ...) {
+    (void)self;
+    (void)fmt;
+}
+
+Backend backend_init(Allocator a) {
+    Backend self = {0};
+    (void)a;
+
+    self.ctx = NULL;
+    self.input = dummy_input;
+    self.render = dummy_render;
+    self.deinit = dummy_deinit;
+    self.finish_rendering = dummy_finish_rendering;
+    self.begin_rendering = dummy_begin_rendering;
+    self.display = dummy_display;
+
+    return self;
+}
--- src/main.c
+++ src/main.c
@@ -25,15 +25,132 @@
     #include "ncurses_backend.c"
 #elif defined(BACKEND_ANSI)
     #include "ansi_backend.c"
+#elif defined(BACKEND_DUMMY) 
+    #include "dummy_backend.c"
 #else
     #warning "No backend specified, defaulting to ANSI"
     #include "ansi_backend.c"
 #endif /*BACKEND*/
 
 
+/*forward declarations*/
+struct Scene;
+struct RunTime;
+struct Entity;
+static int valid_destination(struct Scene * const s, short x, short y);
+
+typedef struct Id {
+    short i;
+} Id;
+const Id null_id = {0};
+
+
+
+
+typedef struct Point {
+    short x;
+    short y;
+} Point;
+const Point direction_offsets[4] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+short point_distance(Point a, Point b) {
+    short result = 0; 
+    result += abs(a.x - b.x);
+    result += abs(a.y - b.y);
+    return result;
+}
+
+
+#define max_path_len 128
+typedef StaticVec(Point, max_path_len) Path;
+
+typedef struct {
+    Path path;
+    Point start;
+    Point finish;
+} PathNode;
+
+
+/*void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))*/
+int pathnode_compare(const void * lhs, const void * rhs) {
+    /*Point x = */
+    const PathNode * a = lhs;
+    const PathNode * b = rhs;
+    const short a_distance = point_distance(svec_top(a->path), a->finish);
+    const short b_distance = point_distance(svec_top(b->path), b->finish);
+
+    assert(a->start.x == b->start.x);
+    assert(a->start.y == b->start.y);
+    assert(a->finish.x == b->finish.x);
+    assert(a->finish.y == b->finish.y);
+
+    return a_distance < b_distance;
+}
+
+Path pathfind_to(struct Scene * s, struct RunTime * r, short x, short y, short destx, short desty) {
+    Path empty_path = {0};
+    const unsigned int max_iterations = 1024;
+    unsigned int iterations = 0;
+    StaticVec(PathNode, max_path_len) queue = {0};
+    bool explored[map_width][map_height] = {0};
+
+    /*add initial points*/
+    Point start = {x, y};
+    Point finish = {destx, desty};
+    PathNode first_node = {empty_path, start, finish};
+    svec_append(queue, first_node);
+
+    for(iterations = 0; iterations < max_iterations; ++iterations) {
+        if(queue.len == 0) break;
+        unsigned int dir = 0;
+        const PathNode top = svec_pop(queue);
+        const Point location = top.path.len == 0 ? start : svec_top(top.path);
+        explored[location.x][location.y] = true;
+
+        /* explore in all 4 directions from node*/
+        for(dir = 0; dir < 4; ++dir) {
+
+            /*add new point in direction to the path of a new pathnode*/
+            const Point offset = direction_offsets[dir];
+            const Point new_location = {location.x + offset.x, location.y + offset.y};
+            Path new_path = top.path;
+            svec_append(new_path, new_location);
+
+            /*check if destination has been found*/
+            if(new_location.x == destx && new_location.y == desty) {
+                /*reverse the path*/
+                unsigned int i = 0;
+                for(i = 0; i < new_path.len / 2; ++i) {
+                    Point tmp = new_path.items[i];
+                    svec_set(new_path, new_path.len - 1 - i, tmp);
+                    svec_set(new_path, i, svec_get(new_path, new_path.len - 1 -i));
+                }
+                return new_path;
+            }
+
+            if(valid_destination(s, new_location.x, new_location.y)
+                    && explored[new_location.x][new_location.y] == false) {
+                PathNode new_node = {new_path, top.start, top.finish};
+                svec_append(queue, new_node);
+            }
+        }
+
+        /*sort the queue for priority*/
+        qsort(queue.items, queue.len, sizeof(PathNode), pathnode_compare);
+    }
+
+
+
+    
+    return empty_path; 
+}
+
+/*Scene and entities*/
+
+
 typedef enum {
     player,
-    rat,
+    basic_melee,
     decoration,
 } EntityType;
 
@@ -47,21 +164,20 @@ typedef struct Entity {
 
     short health;
     bool block_movement;
+
+    Path path;
 } Entity;
 
 
-typedef struct {
-    short i;
-} Id;
-const Id null_id = {0};
 
-typedef struct {
+typedef struct Scene {
+    Id player_id;
     bool entity_used[entity_cap];
     Entity entity[entity_cap];
     StaticVec(Id, cache_size) entity_location_cache[map_width][map_height];
 } Scene;
 
-typedef struct {
+typedef struct RunTime{
     Allocator a;
     Backend b;    
     Action input;
@@ -92,13 +208,27 @@ Entity * get_entity(Scene * s, Id id) {
     return &s->entity[id.i];
 }
 
-/*entity implementations*/
-static int valid_destination(Scene * const s, short x, short y) {
+Entity * get_player(Scene * const s) {
+    if(s->player_id.i) {
+        return get_entity(s, s->player_id);
+    } else {
+        return NULL;
+    }
+}
+
+static Entity * get_new_entity(Scene * s) {
+    Id id = new_entity(s);
+    return get_entity(s, id);
+} 
+
+
+/*pathfinding*/
+static int valid_destination(struct Scene * const s, short x, short y) {
     unsigned long i = 0;
     int movement_blocked = 0;
     for(i = 0; i < s->entity_location_cache[x][y].len; ++i) {
         Id id = svec_get(s->entity_location_cache[x][y], i);
-        const Entity * entity = get_entity(s, id);
+        const struct Entity * entity = get_entity(s, id);
         if(entity->block_movement){
             movement_blocked = 1;
         }
@@ -106,12 +236,8 @@ static int valid_destination(Scene * const s, short x, short y) {
     return is_point_on_map(x, y) && (movement_blocked == false);
 }
 
-
-/*specific*/
-void update_player(Scene * s, RunTime * r, Id id) {
-    Entity * entity = get_entity(s, id);
-    const Action action = r->input;
-    switch(action) {
+void move_entity(Scene * s, Entity * entity, Action direction) {
+    switch(direction) {
         case ACTION_UP: 
             if(valid_destination(s, entity->x, entity->y - 1)) {
                 entity->y -= 1;
@@ -133,7 +259,49 @@ void update_player(Scene * s, RunTime * r, Id id) {
             }
             break;
         default:
+            /*this function should only be called with a movement action*/
+            abort();
             break;
+    }
+}
+
+
+/*specific*/
+void update_player(Scene * s, RunTime * r, Id id) {
+    Entity * entity = get_entity(s, id);
+    const Action action = r->input;
+    s->player_id = id;
+    assert(entity->type == player);
+    switch(action) {
+        case ACTION_UP: 
+        case ACTION_DOWN: 
+        case ACTION_LEFT: 
+        case ACTION_RIGHT: 
+            move_entity(s, entity, action);
+            break;
+        default:
+            break;
+    }
+}
+
+
+void update_basic_melee(Scene * s, RunTime * r, Id id) {
+    Entity * entity = get_entity(s, id);
+    Entity * player = NULL;
+    if((player = get_player(s))) {
+        if(entity->path.len == 0) {
+            entity->path = pathfind_to(s, r, entity->x, entity->y, player->x, player->y);
+        }
+        if(entity->path.len != 0) {
+            Point top = svec_pop(entity->path);
+            if(top.x != entity->x) {
+                move_entity(s, entity, top.x < entity->x ? ACTION_LEFT : ACTION_RIGHT);
+            } else if(top.y != entity->y) {
+                move_entity(s, entity, top.y < entity->y ? ACTION_UP: ACTION_DOWN);
+            } else {
+                /*this means x and y are equal*/
+            }
+        }
     }
 }
 
@@ -149,9 +317,20 @@ void update(Scene * s, RunTime * r, Id id) {
     const Entity * entity = get_entity(s, id);
     switch(entity->type) {
         case player: update_player(s, r, id); break;
-        case rat: {} break;
+        case basic_melee: {} update_basic_melee(s, r, id); break;
         case decoration: {} break;
     }
+}
+
+/*creation functions*/
+Entity * create_decoration(Scene * self, short x, short y, char ch, short color) {
+    Entity * entity = get_new_entity(self);
+    entity->x = x;
+    entity->y = y;
+    entity->ch = ch;
+    entity->color = color;
+    entity->type = decoration;
+    return entity;
 }
 
 Scene create_scene(void) {
@@ -165,35 +344,28 @@ Scene create_scene(void) {
     for(x = 0; x < map_width; ++x) {
         for(y = 0; y < map_height; ++y) {
             if(is_point_on_edge(x, y, map_width, map_height)){
-                Id id = new_entity(&result);
-                Entity * entity = get_entity(&result, id);
-                entity->type = decoration;
-                entity->x = x;
-                entity->y = y;
-                entity->ch = '#';
-                entity->color = 200;
+                Entity * entity = create_decoration(&result, x, y, '#', 42);
                 entity->block_movement = true;
             } else {
-                Id id = new_entity(&result);
-                Entity * entity = get_entity(&result, id);
-                entity->type = decoration;
-                entity->x = x;
-                entity->y = y;
-                entity->ch = '.';
-                entity->color = 20;
-                entity->block_movement = false;
+                if(rand() % 5 == 0) { 
+                    create_decoration(&result, x, y, ',', 42);
+                } else if(rand() % 20 == 0) { 
+                    Entity * entity = create_decoration(&result, x, y, '#', 160);
+                    entity->block_movement = true;
+                } else {
+                    create_decoration(&result, x, y, '.', 3);
+                }
             }
         }
     }
 
-    Id id = new_entity(&result);
-    Entity * entity = get_entity(&result, id);
+    Entity * entity = create_decoration(&result, 5, 5, '@', 200);
     entity->type = player;
-    entity->x = 5;
-    entity->y = 5;
-    entity->ch = '@';
-    entity->color = 5;
-    entity->block_movement = false;
+
+    entity = create_decoration(&result, 9, 9, 'r', 3);
+    entity->type = basic_melee;
+    entity = create_decoration(&result, 11, 11, 'r', 3);
+    entity->type = basic_melee;
 
     return result;
 }
